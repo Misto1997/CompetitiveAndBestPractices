625ea6ae2ae9edce05b64bb3f2b14af25d367876
Rahul Rana <rahul_rana@epam.com>
2020-11-21 20:15:21 +0530

    solutions added

---
+++ src/main/java/com/Leetcode/InterviewQuestionsMedium/ArraysAndStrings/SetMatrixZeroes.java
@@ -0,0 +1,248 @@
+package com.Leetcode.InterviewQuestionsMedium.ArraysAndStrings;
+
+import java.io.*;
+import java.util.InputMismatchException;
+
+public class SetMatrixZeroes {
+    private static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+
+        public int[] readIntArray(int n) {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = readInt();
+            }
+            return a;
+        }
+
+        public char[] readCharArray(int n) {
+            char a[] = new char[n];
+            for (int i = 0; i < n; i++)
+                a[i] = readString().charAt(0);
+            return a;
+        }
+
+        public double[] readDoubleArray(int n) {
+            double a[] = new double[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = readDouble();
+            }
+            return a;
+        }
+
+        public long[] readLongArray(int n) {
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = readLong();
+            }
+            return a;
+        }
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public double readDouble() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.') {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, readInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, readInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public long readLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+
+    public static void main(String[] args) {
+        InputReader input = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+        int m = input.readInt();
+        int n = input.readInt();
+        int[][] ar = new int[m][n];
+        for (int i = 0; i < m; i++)
+            ar[i] = input.readIntArray(n);
+        setMatrix0(ar);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(ar[i][j] + " ");
+            System.out.println();
+        }
+
+        out.close();
+    }
+
+    private static void setMatrix0(int[][] ar) {
+        boolean r = false, c = false;
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = 0; j < ar[0].length; j++) {
+                if (ar[i][j] == 0) {
+                    if (i == 0) r = true;
+                    if (j == 0) c = true;
+                    ar[i][0] = 0;
+                    ar[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < ar.length; i++) {
+            for (int j = 1; j < ar[0].length; j++) {
+                if (ar[i][0] == 0 || ar[0][j] == 0)
+                    ar[i][j] = 0;
+            }
+        }
+        if (r) {
+            for (int j = 0; j < ar[0].length; j++) {
+                ar[0][j] = 0;
+            }
+        }
+        if (c) {
+            for (int i = 0; i < ar.length; i++) {
+                ar[i][0] = 0;
+            }
+        }
+    }
+}
+
