1047e5e84e44ce3358158168c14e520523ca1cd5
Rahul Rana <rahul_rana@epam.com>
2020-11-29 20:26:12 +0530

    more solution added

---
+++ src/main/java/com/Leetcode/InterviewQuestionsMedium/LinkedList/LinkedList.java
@@ -0,0 +1,13 @@
+package com.Leetcode.InterviewQuestionsMedium.LinkedList;
+
+public class LinkedList {
+    int val;
+    LinkedList next;
+
+    LinkedList() {
+    }
+
+    LinkedList(int val) {
+        this.val = val;
+    }
+}
